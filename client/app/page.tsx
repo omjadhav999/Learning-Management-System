0c990196be6f74f1b403bf2d887624d5ba1782ab
Om Jadhav <jadhavom229@gmail.com>
2025-08-28 14:07:07 +0530

    deployement debug

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,46 +1,44 @@
-// "use client";
-// import React, { FC, useEffect, useState } from "react";
-// import Heading from "./utils/Heading";
-// import Header from "./components/Header";
-// import Hero from "./components/Route/Hero";
-// import Courses from "./components/Route/Courses";
-// import Reviews from "./components/Route/Reviews";
-// import FAQ from "./components/FAQ/FAQ";
-// import Footer from "./components/Footer";
+"use client";
+import React, { FC, useEffect, useState } from "react";
+import Heading from "./utils/Heading";
+import Header from "./components/Header";
+import Hero from "./components/Route/Hero";
+import Courses from "./components/Route/Courses";
+import Reviews from "./components/Route/Reviews";
+import FAQ from "./components/FAQ/FAQ";
+import Footer from "./components/Footer";
 
-// interface Props { }
+interface Props { }
 
-// const Page: FC<Props> = (props) => {
-//   const [open, setOpen] = useState(false);
-//   const [activeItem, setActiveItem] = useState(0);
-//   const [route, setRoute] = useState("Login");
+const Page: FC<Props> = (props) => {
+  const [open, setOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState(0);
+  const [route, setRoute] = useState("Login");
 
-//   return (
-//     <>
-//       <Heading
-//         title="ELearning"
-//         description="ELearning is a platform for students to learn and get help from teachers"
-//         keywords="Prograaming,MERN,Redux,Machine Learning"
-//       />
-//       <div>
-//         <Header
-//           open={open}
-//           setOpen={setOpen}
-//           activeItem={activeItem}
-//           setRoute={setRoute}
-//           route={route}
-//         />
-//         <Hero />
-//         <Courses />
-//         <Reviews />
-//         <FAQ />
-//         <Footer />
-//       </div>
-//     </>
-//   );
-// };
+  return (
+    <>
+      <Heading
+        title="ELearning"
+        description="ELearning is a platform for students to learn and get help from teachers"
+        keywords="Prograaming,MERN,Redux,Machine Learning"
+      />
+      <div>
+        <Header
+          open={open}
+          setOpen={setOpen}
+          activeItem={activeItem}
+          setRoute={setRoute}
+          route={route}
+        />
+        <Hero />
+        <Courses />
+        <Reviews />
+        <FAQ />
+        <Footer />
+      </div>
+    </>
+  );
+};
 
-// export default Page;
-export default function Page() {
-  return <h1>Hello from App Router 🎉</h1>;
-}
+export default Page;
+
